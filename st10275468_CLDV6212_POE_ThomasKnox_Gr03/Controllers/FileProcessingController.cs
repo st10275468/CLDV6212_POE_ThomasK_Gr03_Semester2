cd0e48895f514d7025084305620efadd536c03a9
st10275468 <st10275468@vcconnect.edu.za>
2024-08-26 13:37:37 +0200

    Added pages and controllers

---
+++ st10275468_CLDV6212_POE_ThomasKnox_Gr03/Controllers/FileProcessingController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace st10275468_CLDV6212_POE_ThomasKnox_Gr03.Controllers
+{
+    public class FileProcessingController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
