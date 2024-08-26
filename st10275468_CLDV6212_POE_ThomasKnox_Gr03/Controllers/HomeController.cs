6e269ba9d5415d68f586c369b80a77569572bc36
st10275468 <st10275468@vcconnect.edu.za>
2024-08-26 13:07:55 +0200

    Add project files.

---
+++ st10275468_CLDV6212_POE_ThomasKnox_Gr03/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+using Microsoft.AspNetCore.Mvc;
+using st10275468_CLDV6212_POE_ThomasKnox_Gr03.Models;
+using System.Diagnostics;
+
+namespace st10275468_CLDV6212_POE_ThomasKnox_Gr03.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+       
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ st10275468_CLDV6212_POE_ThomasKnox_Gr03/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace st10275468_CLDV6212_POE_ThomasKnox_Gr03.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
