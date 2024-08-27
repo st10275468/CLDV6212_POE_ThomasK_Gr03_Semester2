08ae57e7c2227b0a05eaec3c1c5e033e1287dfb7
st10275468 <st10275468@vcconnect.edu.za>
2024-08-27 10:23:49 +0200

    Added customer details table to the datamanagement page

--- st10275468_CLDV6212_POE_ThomasKnox_Gr03/Controllers/DataManagementController.cs
+++ st10275468_CLDV6212_POE_ThomasKnox_Gr03/Controllers/DataManagementController.cs
@@ -1,12 +1,32 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using st10275468_CLDV6212_POE_ThomasKnox_Gr03.Models;
+using st10275468_CLDV6212_POE_ThomasKnox_Gr03.Services;
 
 namespace st10275468_CLDV6212_POE_ThomasKnox_Gr03.Controllers
 {
     public class DataManagementController : Controller
     {
+        private readonly AzureTableStorageService _azureTableStorageService;
+
+        public DataManagementController(AzureTableStorageService azureTableStorageService)
+        {
+            _azureTableStorageService = azureTableStorageService;
+        }
+
         public IActionResult Index()
         {
             return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> AddCustomerDetails(CustomerDetails customer)
+        {
+            if (ModelState.IsValid)
+            {
+                await _azureTableStorageService.AddEntityAsync(customer);
+
+            }
+            return RedirectToAction("Index");
         }
     }
 }
