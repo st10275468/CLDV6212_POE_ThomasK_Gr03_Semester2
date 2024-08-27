f4ae7408a1a12a0ef4bd4c9b6b4f4161b8331177
st10275468 <st10275468@vcconnect.edu.za>
2024-08-27 10:14:49 +0200

    Integrating the table service onto the table service class

--- st10275468_CLDV6212_POE_ThomasKnox_Gr03/Program.cs
+++ st10275468_CLDV6212_POE_ThomasKnox_Gr03/Program.cs
@@ -1,27 +1,44 @@
-var builder = WebApplication.CreateBuilder(args);
+using st10275468_CLDV6212_POE_ThomasKnox_Gr03.Services;
 
-// Add services to the container.
-builder.Services.AddControllersWithViews();
-
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
+namespace st10275468_CLDV6212_POE_ThomasKnox_Gr03
 {
-    app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
+
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+            builder.Services.AddControllersWithViews();
+
+            builder.Services.AddSingleton<AzureTableStorageService>();
+            builder.Services.AddSingleton<AzureBlobStorageService>();
+            builder.Services.AddSingleton<AzureFileService>();
+            builder.Services.AddSingleton<AzureQueueService>();
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (!app.Environment.IsDevelopment())
+            {
+                app.UseExceptionHandler("/Home/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.MapControllerRoute(
+                name: "default",
+                pattern: "{controller=Home}/{action=Index}/{id?}");
+
+            app.Run();
+        }
+    }
 }
-
-app.UseHttpsRedirection();
-app.UseStaticFiles();
-
-app.UseRouting();
-
-app.UseAuthorization();
-
-app.MapControllerRoute(
-    name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
-
-app.Run();
--- st10275468_CLDV6212_POE_ThomasKnox_Gr03/Services/AzureTableStorageService.cs
+++ st10275468_CLDV6212_POE_ThomasKnox_Gr03/Services/AzureTableStorageService.cs
@@ -1,6 +1,25 @@
-﻿namespace st10275468_CLDV6212_POE_ThomasKnox_Gr03.Services
+﻿
+using Azure;
+using Azure.Data.Tables;
+using st10275468_CLDV6212_POE_ThomasKnox_Gr03.Models;
+using System.Threading.Tasks;
+namespace st10275468_CLDV6212_POE_ThomasKnox_Gr03.Services
 {
     public class AzureTableStorageService
     {
+        private readonly TableClient _tableClient;
+
+        public TableService(IConfiguration configuration)
+        {
+            var connectionString = configuration[""];
+            var serviceClient = new TableServiceClient(connectionString);
+            _tableClient = serviceClient.GetTableClient("CustomerDetails");
+            _tableClient.CreateIfNotExists();
+        }
+
+        public async Task AddEntityAsync(CustomerDetails customer)
+        {
+            await _tableClient.AddEntityAsync(customer);    
+        }
     }
 }
