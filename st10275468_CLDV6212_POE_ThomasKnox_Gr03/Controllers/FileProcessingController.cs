94143db8d69195492c41d1bbf782015e1d74e000
st10275468 <st10275468@vcconnect.edu.za>
2024-08-28 10:48:44 +0200

    Completed Blob storage service. All is working. Just need to change layout and display

--- st10275468_CLDV6212_POE_ThomasKnox_Gr03/Controllers/FileProcessingController.cs
+++ st10275468_CLDV6212_POE_ThomasKnox_Gr03/Controllers/FileProcessingController.cs
@@ -1,15 +1,15 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using st10275468_CLDV6212_POE_ThomasKnox_Gr03.Services;
-
 namespace st10275468_CLDV6212_POE_ThomasKnox_Gr03.Controllers
 {
     public class FileProcessingController : Controller
     {
         private readonly AzureFileService _azureFileService;
-
-        public FileProcessingController(AzureFileService azureFileService)
+        private readonly AzureBlobStorageService _azureBlobStorageService;
+        public FileProcessingController(AzureFileService azureFileService, AzureBlobStorageService azureBlobStorageService)
         {
             _azureFileService = azureFileService;
+            _azureBlobStorageService = azureBlobStorageService;
         }
 
         public IActionResult FileProcessing()
@@ -23,6 +23,18 @@ namespace st10275468_CLDV6212_POE_ThomasKnox_Gr03.Controllers
             {
                 using var stream = file.OpenReadStream();
                 await _azureFileService.UploadFileAsync("file-storage", file.FileName, stream);
+
+            }
+            return View("FileProcessing");
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> UploadMedia(IFormFile file)
+        {
+            if (file != null)
+            {
+                using var stream = file.OpenReadStream();
+                await _azureBlobStorageService.UploadBlobAsync("multimedia-blob-storage", file.FileName, stream);
 
             }
             return View("FileProcessing");
--- st10275468_CLDV6212_POE_ThomasKnox_Gr03/Services/AzureBlobStorageService.cs
+++ st10275468_CLDV6212_POE_ThomasKnox_Gr03/Services/AzureBlobStorageService.cs
@@ -1,6 +1,29 @@
-﻿namespace st10275468_CLDV6212_POE_ThomasKnox_Gr03.Services
+﻿using Azure.Storage.Blobs;
+using Azure.Storage.Files.Shares;
+
+namespace st10275468_CLDV6212_POE_ThomasKnox_Gr03.Services
 {
     public class AzureBlobStorageService
     {
+        private readonly BlobServiceClient _blobServiceClient;
+
+        public AzureBlobStorageService(IConfiguration configuration)
+        {
+
+            if (configuration == null) { 
+
+            throw new ArgumentNullException(nameof(configuration));
+        }
+            _blobServiceClient = new BlobServiceClient(configuration["AzureStorage:ConnectionString"]);
+        }
+
+        public async Task UploadBlobAsync(string sName, string fName, Stream content)
+        {
+            var containerClient = _blobServiceClient.GetBlobContainerClient(sName);
+            await containerClient.CreateIfNotExistsAsync();
+            var blobClient = containerClient.GetBlobClient(fName);
+            await blobClient.UploadAsync(content, true);
+        }
+
     }
 }
